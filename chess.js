a1387fad2396e16572c319a88241312ed734dc69
theoharfordtapp <80641740+theoharfordtapp@users.noreply.github.com>
2024-07-30 14:20:58 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -187,7 +187,7 @@ function checkValidMove(piece, oldSquare, newSquare) {
 function select(square, row, col) {
     const piece = initialBoard[row][col];
     console.log(`Selected Piece: ${selectedPiece} | Selected Square: ${selectedSquare} | Selected Old Square: ${selectedOldSquare}`);
-    if (currentTurn == null || ((piece == piece.toLowerCase()) == currentTurn && piece !== ' ')) {
+    if (currentTurn == null || ((piece == piece.toLowerCase()) == currentTurn || piece == ' ')) {
         if (selectedOldSquare !== null && selectedSquare == null) {
             if (checkValidMove(selectedPiece, selectedOldSquare, square) == true) {
                 console.log(`Selecting square`);
