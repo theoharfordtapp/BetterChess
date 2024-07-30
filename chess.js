fe3b2b4acea4a6451fe6fbc1fcac6ed28014970b
theoharfordtapp <80641740+theoharfordtapp@users.noreply.github.com>
2024-07-30 12:10:02 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,16 +1,16 @@
 const pieces = {
-    'r': 'rook_black.png',
-    'n': 'knight_black.png',
-    'b': 'bishop_black.png',
-    'q': 'queen_black.png',
-    'k': 'king_black.png',
-    'p': 'pawn_black.png',
-    'R': 'rook_white.png',
-    'N': 'knight_white.png',
-    'B': 'bishop_white.png',
-    'Q': 'queen_white.png',
-    'K': 'king_white.png',
-    'P': 'pawn_white.png'
+    'r': 'bR.png',
+    'n': 'bN.png',
+    'b': 'bB.png',
+    'q': 'bQ.png',
+    'k': 'bK.png',
+    'p': 'bP.png',
+    'R': 'wR.png',
+    'N': 'wN.png',
+    'B': 'wB.png',
+    'Q': 'wQ.png',
+    'K': 'wK.png',
+    'P': 'wP.png'
 };
 
 const initialBoard = [
@@ -46,7 +46,7 @@ function placePieces() {
             const piece = initialBoard[row][col];
             if (piece !== ' ') {
                 const img = document.createElement('img');
-                img.src = `assets/${pieces[piece]}`;
+                img.src = `https://images.chesscomfiles.com/chess-themes/pieces/neo/150/${pieces[piece]}`;
                 img.className = 'piece';
                 board[row * 8 + col].appendChild(img);
             }
