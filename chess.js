1aa764a106c64726025407b0e3ade533ce2f3e0c
theoharfordtapp <80641740+theoharfordtapp@users.noreply.github.com>
2024-07-31 14:42:25 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -287,7 +287,11 @@ function checkValidMove(boardState, testingCheck, piece, oldSquare, newSquare) {
     if (pieceType === 'o') {
         const rowDiff = Math.abs(newRow - oldRow);
         const colDiff = Math.abs(newCol - oldCol);
-        return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2) || isPathClear(oldRow, oldCol, newRow, newCol);
+        if (newRow === oldRow || newCol === oldCol) {
+            return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2) || isPathClear(oldRow, oldCol, newRow, newCol);
+        } else {
+            return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2)
+        }
     }
 
     // Bishop moves
